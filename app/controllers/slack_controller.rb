3caa2d83d270bb644f44269a5cd268cbf3438964
Andres <aeum3893@gmail.com>
2023-03-01 13:56:05 -0500

    slack notificator sends message when message is of type spam

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,4 +1,17 @@
 class SlackController < ApplicationController
   def create
+    return unless spam?
+    title = "*#{params["Name"]}*\n"
+    from = "From: #{params["From"]}\n"
+    to = "To: #{params["Email"]}\n"
+    description = "#{params["Description"]}"
+    message = title + from + to + description
+    SlackNotifier::CLIENT.ping message
+  end
+
+  private 
+
+  def spam?
+    params["Type"] == "SpamNotification"
   end
 end
