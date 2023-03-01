4d0109a921f708b4a9198d5bd4b3c643dab066db
Andres <aeum3893@gmail.com>
2023-03-01 16:05:55 -0500

    slack controller tests passes

--- test/controllers/slack_controller_test.rb
+++ test/controllers/slack_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class SlackControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get slack_create_url
+  test "should post create" do
+    post slack_create_url
     assert_response :success
   end
 end
